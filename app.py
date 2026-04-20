239cdd5b4a597b97ff2eb1573ab98508c910e2aa
FrancoCastrillo1 <franco.castrillo1@gmail.com>
2026-04-19 23:08:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def login():
             session["user_email"]  = user["email"]
             session["user_nombre"] = user["nombre"]
             flash("¡Bienvenido de nuevo!")
-            return redirect("/viajes")
+            return redirect("/index")
         else:
             flash("Email o contraseña incorrectos.")
     return render_template("login.html")
